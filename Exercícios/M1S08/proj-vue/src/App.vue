dc03d30056d4a8dcd3535dbe2b17161f2af9634a
Sartori-Dante <135287807+Sartori-Dante@users.noreply.github.com>
2023-08-03 03:50:38 -0300

    update ?

--- Exercícios/M1S08/proj-vue/src/App.vue
+++ Exercícios/M1S08/proj-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Subject: {{ name }}</h1>
+    <!-- <h1>Subject: {{ name }}</h1>
     <p>Id Number: {{ id }}</p>
     <p>Curent Energy Balance: {{ balance }}</p>
     <p>New Energy Balance: {{ balance + balance/5 }}</p>
@@ -8,38 +8,77 @@
     <h2>Consumption Factor Calculator</h2>
     <input type="number" placeholder="set energy usage in Kw/h" v-model="energyUsage">
     <input type="number" placeholder="set time of surge in seconds" v-model="timeSurge">
-    <button @click="calculate()">Calculate</button>
+    <button @click="calculate()">Calculate</button> -->
     <!-- tb funciona >>> v-on:click -->
-    <p>Consumption Average: {{ consumption }} ECA</p>
+    <!-- <p>Consumption Average: {{ consumption }} ECA</p> -->
+
+    <h1>Lista de Cursos</h1>
+    <Card 
+      :titulo="listaCursos[0].titulo"
+      :horas="listaCursos=[0].horas"
+      :nVagas="listaCursos=[0].nVagas"
+      :notas="listaCursos=[0].nota"
+    />
   </div>
 </template>
 
 <script>
+import Card from './components/Card.vue';
 export default {
-  data(){
-    return{
-      name: "Dante_Mcleod",
-      id: 238794,
-      balance: 100,
-      consumption: "",
-      energyUsage: 0,
-      timeSurge: 0
-
-    }
+  components: {
+    Card
   },
-  methods: {
-    calculate(){
-      if(this.energyUsage == 0 || this.timeSurge == 0){
-        alert("Pleas fill both fields")
-        return
-      }
-
-      this.consumption = (this.energyUsage/this.timeSurge)
+  data() {
+    return {
+      listaCursos: [
+        {
+          titulo: "HTML e CSS",
+          horas: "30",
+          nVagas: 40,
+          nota: 9.13
+        },
+        {
+          titulo: "PHP",
+          horas: 20,
+          nVagas: 40,
+          nota: 8.19
+        },
+        {
+          titulo: "VUE.JS",
+          horas: 50,
+          nVagas: 20,
+          nota: 7.43
+        },
+      ]
     }
   }
 }
+
+// export default {
+//   data(){
+//     return{
+//       name: "Dante_Mcleod",
+//       id: 238794,
+//       balance: 100,
+//       consumption: "",
+//       energyUsage: 0,
+//       timeSurge: 0
+
+//     }
+//   },
+//   methods: {
+//     calculate(){
+//       if(this.energyUsage == 0 || this.timeSurge == 0){
+//         alert("Pleas fill both fields")
+//         return
+//       }
+
+//       this.consumption = (this.energyUsage/this.timeSurge)
+//     }
+//   }
+// }
 </script>
 
-<style>
+<style scoped>
 
 </style>
---
+++ Exercícios/M1S08/proj-vue/src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Título: {{titulo}}</h2>
+        <span>Duração: {{horas}}</span>
+        <span>Vagas: {{nVagas}}</span>
+        <span>Nota Geral: {{nota}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['titulo', 'horas', 'nVagas', 'nota']
+}
+</script>
