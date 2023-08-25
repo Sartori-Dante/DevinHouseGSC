1a4ae4877a94bfe33375ee3e90500e33362a2c1c
Sartori-Dante <135287807+Sartori-Dante@users.noreply.github.com>
2023-08-24 22:02:04 -0300

    update format valores

--- Exercícios/M1S12/WebMarket/src/views/Home/Home.vue
+++ Exercícios/M1S12/WebMarket/src/views/Home/Home.vue
@@ -31,13 +31,13 @@
     <v-row>
         <v-col v-for="produto of produtos" :key="produto.id" cols="12" sm="6" md="4" lg="3">
          <v-card :title=produto.nome variant="tonal" flat class="text-xs-center ma-5 pa-3" width="320" height="500">
-            <v-img :width="320" :height="320" aspect-ratio="1/1" :src="produto.imagem"></v-img>
+            <v-img width="320" height="320" aspect-ratio="1/1" :src="produto.imagem"></v-img>
             <!-- <v-responsive class="pt-4">
               
             </v-responsive> -->
             <v-cart-text>
-              <div class="subheading font-weight-black text-h4" >Por R$ {{ produto.preco }}</div>
-              <div class="grey--text">10x de R$ {{ produto.parcela }}</div>
+              <div class="subheading font-weight-black text-h4" >Por {{ new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(produto.preco) }}</div>
+              <div class="grey--text">10x de {{ new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(produto.parcela) }}</div>
             </v-cart-text>
             <v-card-actions>
               <v-btn dark color="blue">
@@ -62,11 +62,11 @@ export default {
       produtos: [
         // {
         //   id: 1,
-        //   nome: 'Cocaína'
+        //   nome: 'Funko 01'
         // },
         // {
         //   id: 2,
-        //   nome: 'SpeedBall'
+        //   nome: 'Funko 02'
         // }
       ]
 
@@ -79,6 +79,7 @@ export default {
       this.produtos = res.data;
     } catch (e) {
       console.error(e);
+      alert("Falha ao recuperar os produtos da database")
     }
   }
 }
